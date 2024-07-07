28d41b23a46ce02176f7ae12bd4f210b9260608b
hardik200404 <Polenati.hardik@gmail.com>
2024-07-07 10:58:26 +0530

    small changes in util files

--- util/database.js
+++ util/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
+require('dotenv').config()
 
-const sequelize = new Sequelize('expense_db','root','',{
+const sequelize = new Sequelize('expense_db','root',process.env.db_password,{
     host:'localhost',
     dialect:'mysql'
 })
--- util/jwt.js
+++ util/jwt.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 function generate_jwt_token(payload){
-    let token=jwt.sign(payload,'SECRET')
+    let token=jwt.sign(payload,process.env.jwt_secret)
     return token
 }
 
 function verify_jwt_token(token){
-    let decoded_id = jwt.verify(token,'SECRET')
+    let decoded_id = jwt.verify(token,process.env.jwt_secret)
     return decoded_id
 }
 
