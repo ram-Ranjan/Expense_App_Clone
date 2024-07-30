03723d4cd7a405a02a8ad0b7f7021c72ac41f02e
hardik200404 <Polenati.hardik@gmail.com>
2024-07-30 11:11:17 +0530

    minor changes

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize('expense_db','root',process.env.db_password,{
-    host:'localhost',
+const sequelize = new Sequelize(process.env.db_name, process.env.db_user, process.env.db_password,{
+    host:process.env.db_host,
     dialect:'mysql',
     timezone: '+05:30', // Indian Standard Time (IST)
     dialectOptions: {
