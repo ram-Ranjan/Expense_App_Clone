76ed49c5c53602b757303cd084b78f829e615c02
hardik200404 <Polenati.hardik@gmail.com>
2024-07-31 11:09:46 +0530

    trying to serve .html files through localhost

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express')
 let helmet = require('helmet')
 let morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 const sequelize = require('./util/database')
 const user_model = require('./models/user_model')
 const expense_model = require('./models/expense_model')
@@ -12,7 +13,27 @@ require('dotenv').config
 let app = express()
 
 app.use(cors())
-app.use(helmet())
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "https://cdnjs.cloudflare.com",
+                "https://checkout.razorpay.com",
+                "https://api.razorpay.com",
+                "https://cdn.jsdelivr.net"
+            ],
+            frameSrc: [
+                "'self'", 
+                "https://checkout.razorpay.com", 
+                "https://api.razorpay.com"
+            ],
+            connectSrc: [
+                "'self'",
+                "https://api.razorpay.com"
+            ],
+        }
+    }
+}))
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static('views/user_views'))
@@ -24,6 +45,10 @@ app.get('/',(req,res)=>{
 
 require('./routes/user_routes')(app)
 require('./routes/expense_routes')(app)
+
+app.use((req,res)=>{
+    res.sendFile(path.join(__dirname,`views/${req.url}`))
+})
 
 user_model.hasMany(expense_model)
 expense_model.belongsTo(user_model)
