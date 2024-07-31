ca40864aeeb2ce36141c3dabb9814464f6457859
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-07-31 12:17:12 +0530

    fix: resolved frontend by removing helmet and morgan added static rendering of views.

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 let express = require('express')
-let helmet = require('helmet')
-let morgan = require('morgan')
+// let helmet = require('helmet')
+// let morgan = require('morgan')
 const cors = require('cors')
 const sequelize = require('./util/database')
 const user_model = require('./models/user_model')
 const expense_model = require('./models/expense_model')
 const order_model = require('./models/order_model')
 const forgot_password_req_model = require('./models/forgot_password_req_model')
+const path = require('path');
+
 require('dotenv').config
 
 let app = express()
 
+app.use(express.static(path.join(__dirname,'views')))
+
+
 app.use(cors())
-app.use(helmet())
-app.use(morgan('combined'))
+// app.use(helmet())
+// app.use(morgan('combined'))
 app.use(express.json())
-app.use(express.static('views/user_views'))
 
 app.get('/',(req,res)=>{
     console.log("hello")
